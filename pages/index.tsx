ff8da7cd72506b90b28e50d1dd9820bc74ca6338
Alex Lee <alex@spoonradio.co>
2023-06-12 14:54:44 +0900

    Use no revalidate

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,8 @@ export const getStaticProps = async () => {
   try {
     const props = await resolveNotionPage(domain)
 
-    return { props, revalidate: 10 }
+    return { props }
+    // return { props, revalidate: 10 }
   } catch (err) {
     console.error('page error', domain, err)
 
