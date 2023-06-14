aac77b55a2c5eecc6c9c12896502c3ec4bfb9ffa
Alex Lee <alex@spoonradio.co>
2023-06-14 18:10:29 +0900

    Add to-dos

--- pages/api/notion/[databaseId].tsx
+++ pages/api/notion/[databaseId].tsx
@@ -11,30 +11,23 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const database_id = (Array.isArray(databaseId)) ? databaseId[0] : databaseId
 
   let result
+  // TODO: Support GET filtering and sorting
   if (req.method === 'GET') {  
-    result = await notion.databases.query({
+  // TODO: Support singular get
+  result = await notion.databases.query({
       database_id
     })  
   }
 
   if (req.method === 'POST') {
     const body = req.body
-    console.log('b', body)
     result = await notion.pages.create({
       parent: { database_id },
       properties: body
-        
-        // title: {
-        //   title:[
-        //     {
-        //       "text": {
-        //         "content": 'ok'
-        //       }
-        //     }
-        //   ]
-        // }
-      ,
     })
   }
+
+  // TODO: Support singular put and delete
+
   return res.status(200).json({ result })
 }
